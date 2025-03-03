96cbf842952f75ff342bf4feaa5cc0df22c67284
Mike Auchterlonie <MikeA@ifa.com>
2025-03-02 20:13:57 -0600

    helmet

--- src/App.tsx
+++ src/App.tsx
@@ -7,12 +7,37 @@ import MyMapComponent from "./components/GoogleMap";
 // import Pricing from "./components/Pricing";
 //import VideoLoop from "./components/VideoLoop";
 import './App.css'
+import { Helmet } from 'react-helmet';
 
 function App() {
-  
+  const structuredData = {
+    "@context": "https://schema.org",
+    "@type": "WebPage",
+    "name": "Pressure Wash - Franklin, TN",
+    "url": "https://pressurewashfranklintn.com",
+    "description": "Pressure washing services, driveways, walkways, siding and decking.",
+    "publisher": {
+      "@type": "Organization",
+      "name": "PressureWashFranklinTN.com",
+      "logo": {
+        "@type": "ImageObject",
+        "url": "https://pressurewashfranklintn.com/images/logo2.svg"
+      }
+    }
+  };
 
   return (
     <>
+      <Helmet>
+        <script type="application/ld+json">{JSON.stringify(structuredData)}</script>
+        <meta name="description" content="Pressure washing services, driveways, walkways, siding and decking." />
+        <meta name="keywords" content="Pressure Wash, Power Wash, Franklin, TN, Tennessee" />
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content="Pressure Wash - Franklin, TN" />
+        <meta property="og:description" content="Pressure washing services, driveways, walkways, siding and decking." />
+        <meta property="og:url" content="https://pressurewashfranklintn.com" />
+        <meta property="og:image" content="https://pressurewashfranklintn.com/images/logo2.svg" />
+      </Helmet>
       <a id="top"></a>
      <Menu /> 
       <div className="container-fluid m-0 p-0" >
